ba9d875f738c49413ab5280f0d666351e29bd43f
dequar <thisdequar@gmail.com>
2026-01-26 20:13:54 +0300

    first commit

---
+++ build/CMakeFiles/GenaXIDE.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/qyour/Documents/GenXIDE/lib/glad/src/glad.c" "CMakeFiles/GenaXIDE.dir/lib/glad/src/glad.c.o" "gcc" "CMakeFiles/GenaXIDE.dir/lib/glad/src/glad.c.o.d"
+  "/Users/qyour/Documents/GenXIDE/lib/imgui/backends/imgui_impl_opengl3.cpp" "CMakeFiles/GenaXIDE.dir/lib/imgui/backends/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/GenaXIDE.dir/lib/imgui/backends/imgui_impl_opengl3.cpp.o.d"
+  "/Users/qyour/Documents/GenXIDE/lib/imgui/backends/imgui_impl_sdl3.cpp" "CMakeFiles/GenaXIDE.dir/lib/imgui/backends/imgui_impl_sdl3.cpp.o" "gcc" "CMakeFiles/GenaXIDE.dir/lib/imgui/backends/imgui_impl_sdl3.cpp.o.d"
+  "/Users/qyour/Documents/GenXIDE/lib/imgui/imgui.cpp" "CMakeFiles/GenaXIDE.dir/lib/imgui/imgui.cpp.o" "gcc" "CMakeFiles/GenaXIDE.dir/lib/imgui/imgui.cpp.o.d"
+  "/Users/qyour/Documents/GenXIDE/lib/imgui/imgui_demo.cpp" "CMakeFiles/GenaXIDE.dir/lib/imgui/imgui_demo.cpp.o" "gcc" "CMakeFiles/GenaXIDE.dir/lib/imgui/imgui_demo.cpp.o.d"
+  "/Users/qyour/Documents/GenXIDE/lib/imgui/imgui_draw.cpp" "CMakeFiles/GenaXIDE.dir/lib/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/GenaXIDE.dir/lib/imgui/imgui_draw.cpp.o.d"
+  "/Users/qyour/Documents/GenXIDE/lib/imgui/imgui_tables.cpp" "CMakeFiles/GenaXIDE.dir/lib/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/GenaXIDE.dir/lib/imgui/imgui_tables.cpp.o.d"
+  "/Users/qyour/Documents/GenXIDE/lib/imgui/imgui_widgets.cpp" "CMakeFiles/GenaXIDE.dir/lib/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/GenaXIDE.dir/lib/imgui/imgui_widgets.cpp.o.d"
+  "/Users/qyour/Documents/GenXIDE/src/source.cpp" "CMakeFiles/GenaXIDE.dir/src/source.cpp.o" "gcc" "CMakeFiles/GenaXIDE.dir/src/source.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/GenaXIDE.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GenaXIDE.dir/lib/glad/src/glad.c.o"
+  "CMakeFiles/GenaXIDE.dir/lib/glad/src/glad.c.o.d"
+  "CMakeFiles/GenaXIDE.dir/lib/imgui/backends/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/GenaXIDE.dir/lib/imgui/backends/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/GenaXIDE.dir/lib/imgui/backends/imgui_impl_sdl3.cpp.o"
+  "CMakeFiles/GenaXIDE.dir/lib/imgui/backends/imgui_impl_sdl3.cpp.o.d"
+  "CMakeFiles/GenaXIDE.dir/lib/imgui/imgui.cpp.o"
+  "CMakeFiles/GenaXIDE.dir/lib/imgui/imgui.cpp.o.d"
+  "CMakeFiles/GenaXIDE.dir/lib/imgui/imgui_demo.cpp.o"
+  "CMakeFiles/GenaXIDE.dir/lib/imgui/imgui_demo.cpp.o.d"
+  "CMakeFiles/GenaXIDE.dir/lib/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/GenaXIDE.dir/lib/imgui/imgui_draw.cpp.o.d"
+  "CMakeFiles/GenaXIDE.dir/lib/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/GenaXIDE.dir/lib/imgui/imgui_tables.cpp.o.d"
+  "CMakeFiles/GenaXIDE.dir/lib/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/GenaXIDE.dir/lib/imgui/imgui_widgets.cpp.o.d"
+  "CMakeFiles/GenaXIDE.dir/src/source.cpp.o"
+  "CMakeFiles/GenaXIDE.dir/src/source.cpp.o.d"
+  "GenaXIDE"
+  "GenaXIDE.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/GenaXIDE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
