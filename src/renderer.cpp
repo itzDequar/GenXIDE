8636fd625ea05178bd261e3f756e5c0c3ca766d0
dequar <thisdequar@gmail.com>
2026-01-28 19:54:49 +0300

    added: renderer.cpp/h (blank)

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -1,1 +1,14 @@
 #include <renderer.h>
+
+bool Renderer::init()
+{
+  bool success{ true };
+
+  return success;
+}
+
+void Renderer::renderer()
+{
+
+  return;
+}
--- src/renderer.h
+++ src/renderer.h
@@ -1,1 +1,13 @@
 #pragma once
+
+class Renderer
+{
+  public:
+  bool init();
+
+  void renderer();
+
+
+  private:
+
+};
